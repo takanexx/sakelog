7f28fce6ac23c2bec5c91138cea2b20d67a46ca7
Gaku Ishikawa <gaku.jp.301@gmail.com>
2025-11-08 06:25:08 +0900

    create start page

--- SakeLog/StartView.swift
+++ SakeLog/StartView.swift
@@ -5,6 +5,7 @@
 //  Created by Takane on 2025/11/06.
 //
 import SwiftUI
+import RealmSwift
 
 struct CarouselView: View {
     let items = ["🍶", "🍷", "🍺", "🥃", "🍸"]
@@ -32,37 +33,56 @@ struct CarouselView: View {
 
 
 struct StartView: View {
-    @State private var currentIndex = 1  // ← 現在のページ
+    @State private var currentIndex = 1  // 現在のページ
+    @State private var navigateToMain = false   // 遷移状態を管理
 
     var body: some View {
-        VStack {
-            Text("Welcome to SakeLog")
-                .font(.largeTitle)
-                .padding()
-            Text("Your personal sake tasting journal.")
-                .font(.subheadline)
-                .padding()
-            CarouselView(currentIndex: $currentIndex)
-
-            Spacer()
+        NavigationStack {
             
-            Text("\(currentIndex)")
-            Button(action: {
-                // Action to get started
-            }) {
-                Text("Get Started")
-                    .font(.headline)
+            VStack {
+                Text("Welcome to SakeLog")
+                    .font(.largeTitle)
                     .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(isAtLastPage ? Color.blue : Color.gray)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                Text("Your personal sake tasting journal.")
+                    .font(.subheadline)
+                    .padding()
+                CarouselView(currentIndex: $currentIndex)
+                
+                Spacer()
+                
+                Text("\(currentIndex)")
+                Button(action: {
+                    // Action to get started
+                    let user = User(
+                        username: "ゲストユーザー",
+                        email: "",
+                    )
+                    
+                    let realm = try! Realm()
+                    try! realm.write {
+                        realm.add(user)
+                    }
+                    // ContentView へ移動する処理をここに追加
+                    navigateToMain = true
+                }) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(isAtLastPage ? Color.blue : Color.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .disabled(!isAtLastPage) // 最後のページまで進まないと押せない
+                .padding(.horizontal)
             }
-            .disabled(!isAtLastPage) // 最後のページまで進まないと押せない
-            .padding(.horizontal)
+            .padding()
+            .animation(.easeInOut, value: currentIndex)
+            .navigationDestination(isPresented: $navigateToMain) {
+                ContentView()
+                    .navigationBarBackButtonHidden(true)
+            }
         }
-        .padding()
-        .animation(.easeInOut, value: currentIndex)
     }
     
     /// 最後のページにいるかどうか
