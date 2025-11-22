dd6b9e05c15ef75a294b8747f9c3abc859a2a1dd
Gaku Ishikawa <gaku.jp.301@gmail.com>
2025-11-22 09:23:39 +0900

    fix home

--- SakeLog/HomeView.swift
+++ SakeLog/HomeView.swift
@@ -9,54 +9,66 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack {
-                    ScrollView {
-                        GeometryReader { geo in
-                            let minY = geo.frame(in: .global).minY
-                            let height = max(600 - minY, 150)  // 縮む上限・下限
-
-                            ZStack(alignment: .bottomLeading) {
-                                ModelRenderView(labelImageName: "izumi", allowsCameraControl: false)
-                                    .frame(height: height)
-                                    .clipped()
-
-                                VStack(alignment: .leading, spacing: 6) {
-                                    Text("Promoted")
-                                        .font(.caption)
-
-                                    Text("Round-the-clock\nwellness")
-                                        .font(.title.bold())
-
-                                    Button("Shop now") { }
-                                        .padding(.horizontal, 20)
-                                        .padding(.vertical, 8)
-                                        .background(.white.opacity(0.9))
-                                        .cornerRadius(20)
-                                }
-                                .padding(20)
-                                .shadow(radius: 5)
+            ScrollView {
+                VStack(spacing: 20) {
+                    GeometryReader { geo in
+                        let minY = geo.frame(in: .global).minY
+                        let height = max(600 + minY, 150)  // 縮む上限・下限
+                        
+                        ZStack(alignment: .bottomLeading) {
+                            ModelRenderView(labelImageName: "izumi", allowsCameraControl: false)
+                                .frame(height: height)
+                                .clipped()
+                            
+                            VStack(alignment: .leading, spacing: 6) {
+                                Text("Promoted")
+                                    .font(.caption)
+                                
+                                Text("Round-the-clock\nwellness")
+                                    .font(.title.bold())
+                                
+                                Button("Shop now") { }
+                                    .padding(.horizontal, 20)
+                                    .padding(.vertical, 8)
+                                    .background(.white.opacity(0.9))
+                                    .cornerRadius(20)
                             }
-                            .background(Color.gray.opacity(0.1))
-                            .frame(height: height)
-                            .offset(y: minY > 0 ? -minY : 0)  // 上に引っ張られた時の処理
+                            .padding(20)
+                            .shadow(radius: 5)
                         }
-                        .frame(height: 600) // ←スクロール前の元高さ
-
-                        // MARK: - SearchBar
-//                        SearchBarView()
-//                            .padding(.horizontal)
-//                            .padding(.top, -10)
-//
-//                        // MARK: - Featured Stores
-//                        FeaturedStoresView()
-//                            .padding(.top, 10)
-
+                        .background(Color.gray.opacity(0.1))
+                        .frame(height: height)
+                        .offset(y: -minY)
                     }
-                    .navigationBarHidden(true)
+                    .frame(height: 400) // ←スクロール前の元高さ
+                    HStack {
+                        Text("Featured Stores")
+                            .frame(height: 400)
+                    }
+                    HStack {
+                        Text("Featured Stores")
+                            .frame(height: 400)
+                            .foregroundColor(.red)
+                    }
                 }
-            
+                
+                
+                // MARK: - SearchBar
+                //                        SearchBarView()
+                //                            .padding(.horizontal)
+                //                            .padding(.top, -10)
+                //
+                //                        // MARK: - Featured Stores
+                //                        FeaturedStoresView()
+                //                            .padding(.top, 10)
+                
+            }
+            .navigationBarHidden(true)
+        }
+        
     }
 }
-            
+
 #Preview {
     HomeView()
 }
