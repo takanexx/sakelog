9d320d13001ee918343be150764302157a4c0b2f
Gaku Ishikawa <gaku.jp.301@gmail.com>
2026-02-10 06:26:20 +0900

    add analytics

--- SakeLog/SakeLogApp.swift
+++ SakeLog/SakeLogApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import RealmSwift
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct SakeLogApp: SwiftUI.App {
+    // Firebase 初期化
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject private var userManager = UserManager()
     
     let realmConfig = Realm.Configuration(
